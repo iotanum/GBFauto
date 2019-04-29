8e0afca69a1268d59c847acfcef93aa9e4ed17ac
iotanum <evalduuk@gmail.com>
2019-04-29 20:36:12 +0300

    Rewrite whole quest on repeat option logic

--- gbf/quest_on_repeat.py
+++ gbf/quest_on_repeat.py
@@ -4,7 +4,6 @@ from bs4 import SoupStrainer as ss
 import os
 import time
 import random
-import re
 
 from selenium import common as selenium_err
 
@@ -13,14 +12,7 @@ class QuestOnRepeat:
     def __init__(self, game_handler):
         self.bot = game_handler
         self.driver = game_handler.driver
-        #######################
-        self.total_ranks = 0
-        self.total_exp = 0
-        self.total_fights = 0
-        self.total_tokens = 0
-        self.total_honors = 0
-        self.total_pendants = 0
-        self.start_time = time.time()
+        self.repeat = False
 
     def wait_for_repeatable_quest(self):
         print('\nWaiting for you to enter a repeatable quest...')
@@ -29,7 +21,7 @@ class QuestOnRepeat:
             if '#quest/supporter' in url:
                 print("Locked in on this quest.")
                 break
-        self.handle_not_enough_ap()
+        # self.handle_not_enough_ap()
 
     def remove_battle_scene_element(self):
         try:
@@ -59,9 +51,10 @@ class QuestOnRepeat:
             else:
                 mobs_alive = False
 
-    def wait_before_fight(self, in_fight=False):
-        if in_fight is True:
+    def wait_before_fight(self, in_fight=False, raid=False):
+        if in_fight is True and raid is False:
             self.bot.wait.for_quest_advencment_screen(start=True)
+
         self.bot.wait.for_loading_screen()
         self.bot.wait.for_quest_advencment_screen()
         self.bot.wait.for_fight_ready_screen()
@@ -73,26 +66,42 @@ class QuestOnRepeat:
         progress_bar = parser.find('div', {'class': 'prt-position'})
         quest_parts = progress_bar.find_all('div', {'class': ['lis-spot']})
 
-        if quest_parts == 0:
-            quest_parts = 1
+        # If list is empty - it's a one fight quest
+        if not quest_parts:
+            quest_parts = [1]
 
         return len(quest_parts)
 
     def handle_fight(self):
         self.wait_before_fight()
-        num_of_fights = self.count_quest_fight_parts()
+
+        # If 'Quest' has backup request screen
+        # then it means that it's a raid.
+        raid = self.bot.handle.backup_request()
+        if raid is False:
+            self.bot.wait.for_quest_advencment_screen(start=True)
+            num_of_fights = self.count_quest_fight_parts()
+        else:
+            num_of_fights = 1
+            # Raid screen takes time to fully disappear
+            time.sleep(0.8)
+
         first_queue_from_config = os.getenv("QUEUE_FIRST_FIGHT")
         second_queue_from_config = os.getenv("QUEUE_SECOND_FIGHT")
         third_queue_from_config = os.getenv('QUEUE_THIRD_FIGHT')
         queues = [first_queue_from_config, second_queue_from_config, third_queue_from_config]
+
         for fight_num, queue in enumerate(queues, 1):
             # Don't need to wait on first iteration
-            if fight_num != 1:
-                self.wait_before_fight(in_fight=True)
+            # TODO less clutter
+            if fight_num != 1 or raid is True:
+                self.wait_before_fight(in_fight=True, raid=True if raid else False)
+
             print(f"Fight #{fight_num}.")
             self.bot.queue.do_queue(queue)
             self.finish_fight()
             self.bot.wait.for_fight_main_mask()
+            time.sleep(1.5)
             self.bot.press.results_button()
             if fight_num == num_of_fights:
                 break
@@ -102,108 +111,49 @@ class QuestOnRepeat:
         self.bot.press.support_element(5)
         time.sleep(0.5)
         self.bot.press.first_support_summon()
+        captcha = self.bot.handle.human_verification()
+        # if verification is required - need to repeat the last step
+        if captcha is True:
+            self.bot.press.first_support_summon()
         self.bot.press.confirm_support_summon()
 
-    def convert_gain_to_int(self, gain):
-        regex_num_pattern = r'\d+'
-        gains = re.findall(regex_num_pattern, gain)
-        gain = [int(s) for s in gains]
-        return sum(gain)
-
-    def convert_seconds_to_hms_format(self, seconds):
-        seconds = round(seconds, 2)
+    def convert_seconds_to_hms_format(self):
+        seconds = round(self.bot.run_time(), 2)
         minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
 
         return int(round(hours, 2)), int(round(minutes, 2)), int(round(seconds, 2))
 
-    def count_after_fight_event_items(self):
-        self.bot.popup.event_items()
-
-        parser = bs(self.driver.page_source, features="lxml")
-        gains = parser.find_all('div', {'class': 'prt-event-point'})
-
-        for gain in gains:
-            if gain is not None:
-                gain_name = str(gain.text)
-                gain_num = self.convert_gain_to_int(gain_name)
-                if 'tokens' in gain_name:
-                    self.total_tokens += gain_num
-                elif 'honors' in gain_name:
-                    self.total_honors += gain_num
-                elif 'pendants' in gain_name:
-                    self.total_pendants += gain_num
-
-        self.bot.press.usual_ok()
-
     def handle_after_fight(self):
         self.bot.wait.for_loading_screen()
 
-        for retry_no in range(3):
-            try:
-                parser = bs(self.driver.page_source, features="lxml")
-                gains = parser.find('div', {'class': 'prt-exp-gain'}).find_all('span')
-                break
-            except AttributeError:
-                time.sleep(3)
-                gains = None
-                continue
+        # After the page loads, there's no way to 'tell' if there will be
+        # x popup or y popup, it load time depends on what popup it is
+        # so plain old 'sleep' will do the trick
+        time.sleep(3)
+        self.bot.handle.after_fight_popups(kill=True)
 
-        if gains:
-            for gain in gains:
-                if gain is not None:
-                    gain_name = str(gain['class'])
-                    gain = self.convert_gain_to_int(gain.text)
-                    if 'rank' in gain_name:
-                        self.total_ranks += gain
-                    elif 'exp' in gain_name:
-                        self.total_exp += gain
-            self.bot.press.usual_ok()
-
-        # ???
-        try:
-            self.count_after_fight_event_items()
-        except selenium_err.exceptions.NoSuchElementException:
-            pass
-
-        self.total_fights += 1
-        run_time = time.time() - self.start_time
-        hours, minutes, seconds = self.convert_seconds_to_hms_format(run_time)
-        print(f"Total fights: {self.total_fights}, EXP: {self.total_exp}, Rank points: {self.total_ranks}\n"
+        hours, minutes, seconds = self.convert_seconds_to_hms_format()
+        avg_time_per_quest = round(self.bot.run_time() / self.bot.total_fights, 2)
+        print(f"Total fights: {self.bot.total_fights}, EXP: {self.bot.total_exp}, Rank points: {self.bot.total_ranks}\n"
               f"Running for {hours}h:{minutes}min:{seconds}s, "
-              f"Average time per quest: {round(run_time / self.total_fights, 2)}s")
-
-        # TODO
-        # If after pressing a button certain popup appears
-        # ignore and/or handle other popups accordingly
-        rank_up = self.bot.popup.new_rank()
-        if rank_up is True:
-            print("New rank!")
-            self.bot.press.usual_ok()
-
-        extended_mastery = self.bot.popup.extended_mastery()
-        if extended_mastery is True:
-            print("New extended mastery!")
-            time.sleep(4)
-            self.driver.find_element_by_id('cjs-lp-rankup').click()
+              f"Average time per quest: {avg_time_per_quest}s")
 
         self.bot.wait.for_loot_screen()
         self.bot.press.play_again_quest()
 
-        achievement = self.bot.popup.achievement()
-        if achievement is True:
-            print('New achievement!')
-            self.bot.press.usual_close()
+        nightmare_battle = self.bot.handle.after_fight_popups()
+        if nightmare_battle is True:
+            self.repeat = False
+            return
 
-        friend_request = self.bot.popup.friend_request()
-        if friend_request is True:
-            self.bot.press.usual_cancel()
-
-        self.handle_not_enough_ap()
+        if '#quest/supporter' not in str(self.driver.current_url):
+            self.handle_not_enough_ap()
 
     def handle_not_enough_ap(self):
         self.bot.wait.for_loading_screen()
         not_enough_ap = self.bot.popup.not_enough_x()
+        print(not_enough_ap, 'ap handle')
         if not_enough_ap is True:
             potion_amount = random.randint(1, 5)
             self.bot.action.use_potions_or_pills(potion_amount)
@@ -212,31 +162,20 @@ class QuestOnRepeat:
 
     def repeatable_quest(self):
         while True:
-            self.do_repeatable_quest(repeat=True if self.total_fights > 0 else False)
+            self.do_repeatable_quest()
 
-    def handle_quest_scrolling_screen(self):
-        self.bot.wait.for_loading_screen()
-        try:
-            self.bot.wait.for_side_scroll_entry()
-            self.bot.press.usual_skip()
-            self.bot.popup.skip_side_scroll()
-            self.driver.find_element_by_xpath('//*[@id="pop"]/div/div[3]/div[2]').click()
-            self.bot.popup.side_scroll_results()
-            self.driver.find_element_by_xpath('//*[@id="pop"]/div/div[3]/div').click()
-            self.bot.wait.for_loading_screen()
-        except selenium_err.exceptions.NoSuchElementException:
-            pass
-
-    def do_repeatable_quest(self, repeat=False):
-        # HANDLE PATH TO REPEATABLE QUEST
-        if repeat is False:
-            self.wait_for_repeatable_quest()
-
-        # SUPPORT SUMMONS
+    def handle_pre_fight(self):
         self.handle_pre_fight_support_summons()
+        self.bot.handle.pre_fight_screens()
 
-        # Scrolling screen
-        self.handle_quest_scrolling_screen()
+    def do_repeatable_quest(self):
+        # HANDLE PATH TO REPEATABLE QUEST
+        if self.repeat is False:
+            self.wait_for_repeatable_quest()
+            self.repeat = True
+
+        # PRE-FIGHT STUFF
+        self.handle_pre_fight()
 
         # FIGHT
         self.handle_fight()
