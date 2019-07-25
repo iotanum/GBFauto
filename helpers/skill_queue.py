bd5e35553cfd6e590e983d9c8e424156d51e4c1c
iotanum <evalduuk@gmail.com>
2019-07-25 19:39:52 +0300

    Add a handle for different type of quests for quests_on_repeat

--- helpers/skill_queue.py
+++ helpers/skill_queue.py
@@ -151,7 +151,7 @@ class Skills:
                 # if char_num is 5, then it means it's a support summon
                 else:
                     # Skip pressing 'back' button if support summon is your starter 'char'
-                    if summon_was_used is True:
+                    if summon_was_used is True or step != 1:
                         self._bot.press.back()
                     actions_for_summon = [self._bot.press.summon_card,
                                           self._bot.press.summon_num,
