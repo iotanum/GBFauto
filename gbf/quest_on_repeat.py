f7437cb3d16d0d1085ffe7b6dd672aad7b9dcc5d
Evaudas Macaitis <evalduuk@gmail.com>
2022-06-25 12:42:04 +0300

    bla

--- gbf/quest_on_repeat.py
+++ gbf/quest_on_repeat.py
@@ -81,7 +81,7 @@ class QuestOnRepeat:
 
             if next_turn_queue and self.auto_button_on:
                 self.bot.handle.check_if_chara_are_attacking()
-            #     self.bot.press.auto_attack()
+                self.bot.press.auto_attack()
                 self.auto_button_on = False
 
             try:
@@ -95,8 +95,8 @@ class QuestOnRepeat:
                         self.auto_button_on = True
 
                 if next_turn_queue and self.auto_button_on:
-                    # print("next turn queue and auto btn on")
-                #     self.bot.press.auto_attack()
+                    print("next turn queue and auto btn on")
+                    self.bot.press.auto_attack()
                     self.auto_button_on = False
 
                 print(battle, "refresh boi")
