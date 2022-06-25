5ca55bea3ef575e671a7afdd7d306300d3b9c444
Evaudas Macaitis <evalduuk@gmail.com>
2022-06-25 13:32:38 +0300

    Update .bat launch script

--- launch.bat
+++ launch.bat
@@ -1,7 +1,7 @@
 @echo off
 
-call python -m venv env
-call .\\env\Scripts\activate
+call python -m venv venv
+call .\\venv\\Scripts\\activate.bat
 call pip install --upgrade pip
 call pip install -r requirements.txt
 call python main.py
