77563204facc9ab915b2c3322d66e88851e5ce55
Evaudas Macaitis <evalduuk@gmail.com>
2025-01-19 21:32:45 +0200

    feat: add windows launch script

---
+++ launch.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call python -m venv venv
+call .\\venv\\Scripts\\activate.bat
+call pip install --upgrade pip
+call pip install -r req
+call playwright install
+call py launch.py
+pause
