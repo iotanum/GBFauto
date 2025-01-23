d422f4bbeac4cf1c4ddef9b7edfef8d0138aaba2
Evaudas Macaitis <evalduuk@gmail.com>
2025-01-23 17:20:48 +0200

    fix: raids end_battle event to result_screen event

--- gbfauto/raids.py
+++ gbfauto/raids.py
@@ -131,7 +131,7 @@ class Raids:
 
         while True:
             _log.debug("Waiting for battle to end...")
-            if await self.events_common.is_event_recent(EventEnums.BATTLE_END_EVENT):
+            if await self.events_common.is_event_recent(EventEnums.RESULT_SCREEN_EVENT):
                 self.navigated_to_raids = False
                 _log.info("Returning to raid filters screen...")
                 return
